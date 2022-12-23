6209af7e15f035d973e13d4421c36b84a91c7656
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-12-23 17:33:44 +0200

    Working

--- src/components/MoviesTrending.jsx
+++ src/components/MoviesTrending.jsx
@@ -1,3 +1,19 @@
-export const MoviesTrending = () => {
-  return <div>List</div>;
+import { Link } from 'react-router-dom';
+import { MovieName, CardWrapper } from './MoviesTrending.styled';
+
+export const MoviesTrending = ({ trandingMovies }) => {
+  return (
+    <ul>
+      <h2>Trending today</h2>
+      {trandingMovies.map(movie => (
+        <CardWrapper key={movie.id}>
+          <img
+            src="https://api.themoviedb.org/3/trending/movie/tQ91wWQJ2WRNDXwxuO7GCXX5VPC.jpg?api_key=00f2cafab9ed8b61ede4a54c54838e2c"
+            alt={movie.title}
+          />
+          <MovieName>{movie.original_title}</MovieName>
+        </CardWrapper>
+      ))}
+    </ul>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import { MoviesTrending } from 'components/MoviesTrending';
+import { useEffect, useState } from 'react';
 
 export const Home = () => {
+  const BASE_URL = 'https://api.themoviedb.org/3';
+  const API_KEY = '00f2cafab9ed8b61ede4a54c54838e2c';
+  const [trandingMovies, setTrandingMovies] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`)
+      .then(responce => {
+        if (responce.ok) {
+          return responce.json();
+        }
+
+        return Promise.reject(new Error('Something has gone wrong!'));
+      })
+      .catch(error => setError({ error }))
+      .then(data => setTrandingMovies(data.results));
+    //    .finally(() => setLoading(false));
+  }, []);
+
   return (
     <main>
-      <MoviesTrending />
+      <MoviesTrending trandingMovies={trandingMovies} />
     </main>
   );
 };
