5d470646d36ab4f94e615893604bf2e92835ef19
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-12-27 00:38:29 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ const NotFound = lazy(() => import('../pages/NotFound'));
 export const App = () => {
   return (
     <Routes>
-      <Route path="/goit-react-hw-05-movies/" element={<SharedLayout />}>
+      <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<MovieDetails />}>
